d78fbae0489cdecfc382d1009d19fc6248cca13c
EnderC00kiez <49785023+EnderC00kiez@users.noreply.github.com>
2021-12-04 14:11:33 -0500

    The sound update

--- main.py
+++ main.py
@@ -4,24 +4,37 @@
 
 import sys
 import os
+import time
 try:
-    import playsound
     import pickle
+    import requests
+    import simpleaudio
+    import shutil
 except:
     if sys.platform == 'win32':
-        os.system('pip install playsound')
+        os.system('pip install simpleaudio')
+        os.system('pip install requests')
+        os.system('pip install shutil')
         os.system('pip install pickle')
-        import playsound
+        import simpleaudio
+        import requests
+        import shutil
         import pickle
     elif sys.platform == 'linux':
-        os.system('pip3 install playsound')
+        os.system('pip3 install simpleaudio')
+        os.system('pip3 install requests')
+        os.system('pip3 install shutil')
         os.system('pip3 install pickle')
-        import playsound
+        import simpleaudio
+        import requests
+        import shutil
         import pickle
     elif sys.platform == 'darwin':
-        os.system('pip3 install playsound')
-        os.system('pip3 install pickle')
-        import playsound
+        os.system('pip3 install simpleaudio')
+        os.system('pip3 install requests')
+        import simpleaudio
+        import requests
+        import shutil
         import pickle
     else:
         print('Unknown OS')
@@ -29,7 +42,7 @@ except:
 import savelib
 
 def setup():
-    musictype = input("Would you like to stream music or download it? (s/d) (for download you need to install Git)")
+    musictype = input("Would you like to stream music or download it? (s/d) (you need to install Git) ")
     if musictype == 's':
         print("Adding streamMusic value to settings.pkl...")
         savelib.add("true")
@@ -54,5 +67,48 @@ except:
     setup()
 
 print("Welcome to Bob Werld!")
+print("NOTE: THIS IS BETA SOFTWARE. USE AT YOUR OWN RISK.")
+print("DEV BUILD 5.2.9")
+disableMusic = False
+if savelib.loadslot(1, "settings.pkl") == 'true':
+    print("Setting streamMusic variable to true")
+    streamMusic = True
+    if requests.get('https://example.com').status_code == 200:
+        print("Internet connection established.")
+    else:
+        print("No internet connection detected. Music will not play.")
+        streamMusic = False
+        disableMusic = True
+
+elif savelib.loadslot(1, "settings.pkl") == 'false':
+    print("Setting streamMusic variable to false")
+    streamMusic = False
+else:
+    print("Your settings.pkl is corrupted, please delete it and run the program again.")
+    exit()
+
+
+if disableMusic == True:
+    print("Detected disableMusic variable, music will not play.")
+else:
+    if streamMusic == True:
+        print("Music will be streamed.")
+        os.system("git clone -b music https://github.com/cobralang/bob-werld.git")
+        os.rename("bob-werld", "music")
+        menu_theme = simpleaudio.WaveObject.from_wave_file("music/bob_world_menu_theme.wav")
+        menyoo = simpleaudio.WaveObject.from_wave_file("music/menyoo.wav")
+        jump = simpleaudio.WaveObject.from_wave_file("music/jump.wav")
+        crash = simpleaudio.WaveObject.from_wave_file("music/crash.wav")
+        buildnbreak = simpleaudio.WaveObject.from_wave_file("music/build-n-break.wav")
+        bob = simpleaudio.WaveObject.from_wave_file("music/Bob.wav")
+        shutil.rmtree("music")
+    elif streamMusic == False:
+        menu_theme = simpleaudio.WaveObject.from_wave_file("music/bob_world_menu_theme.wav")
+        menyoo = simpleaudio.WaveObject.from_wave_file("music/menyoo.wav")
+        jump = simpleaudio.WaveObject.from_wave_file("music/jump.wav")
+        crash = simpleaudio.WaveObject.from_wave_file("music/crash.wav")
+        buildnbreak = simpleaudio.WaveObject.from_wave_file("music/build-n-break.wav")
+        bob = simpleaudio.WaveObject.from_wave_file("music/Bob.wav")
+
 
 
